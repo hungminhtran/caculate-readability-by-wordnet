b76978b3c8a504ada4a64c9fdca366eb1ab29e48
Hùng Minh Trần <Hng Minh Trn>
2016-04-29 01:21:52 +0700

    modified cpRatio to export data like table from experiment 2

--- module-1.py
+++ module-1.py
@@ -1,10 +1,14 @@
 from nltk.corpus import wordnet as wn
+from operator import add
 import nltk
 import re
 import pickle
 
+MAX_LEVEL = 6
+
+#input: noun
+#output: standardized noun
 def standanizeNoun(noun):
-    #standardize noun
     tnoun = noun.replace(' ', '_')
     tnoun = wn.morphy(tnoun, wn.NOUN)
     return tnoun
@@ -12,40 +16,45 @@ def standanizeNoun(noun):
 #compound ration of a word / hyponym
 #input: noun
 #output: (cpd, hyponym, total_hyponym_len)
+#
+# which each nouns, get all hypernym/hyponyms (6 levels max)
+#     --> given a word, calculate number of compounds in full_hypornym
 def cpdRatio(SynsetNoun, level, hypernymName):
-    #maximum is recurse 6 level
-    if (level > 6):
-        return 0, 0, 0
-    if (not SynsetNoun):
-        return 0, 0, 0
 #    print level,SynsetNoun.name()
     cpd = 0
     hyponym = 0
     total_lens = 0
+    cpdByLvls = [0] * (MAX_LEVEL + 1)
+        #maximum is recurse 6 level
+    if (level > MAX_LEVEL or not SynsetNoun):
+        return cpd, hyponym, total_lens, cpdByLvls
+
     allHyponyms = SynsetNoun.hyponyms() #why we choose this?
     if (allHyponyms == []):
-        return 0, 0, 0
+        return cpd, hyponym, total_lens, cpdByLvls
     for synset in allHyponyms:
         for lemma in synset.lemmas():
             hyponym = hyponym + 1
             if re.search(hypernymName, lemma.name()): #noun in cpd hyponym, don't care whole word???
                 cpd = cpd + 1
+                cpdByLvls[level] = cpdByLvls[level] + 1 #count cpd in current level
             total_lens = total_lens + len(lemma.name())
 #            print level, SynsetNoun.hyponyms()[0].name(), lemma.name()
-            if synset.name().find('_'):
-                total_lens = total_lens -1 #remove space from len counter
+            total_lens = total_lens - synset.name().count('_') #remove space from len counter
         if synset.lemmas()[0]:
-            tmp1, tmp2, tmp3 = cpdRatio(synset, level+1, hypernymName)
+            # print cpdRatio(synset, level+1, hypernymName)
+            tmp1, tmp2, tmp3, tmp4 = cpdRatio(synset, level+1, hypernymName)
             cpd = cpd + tmp1
             hyponym = hyponym + tmp2
             total_lens = total_lens + tmp3
+            cpdByLvls = map(add, cpdByLvls, tmp4)
     #return result
-    return cpd, hyponym, total_lens
+    return cpd, hyponym, total_lens, cpdByLvls
 
-#testNoun = ['guitar', 'apples', 'piano', 'drum', 'peach', 'grape', 'hammer', 'saw', 'screwdriver', 'pants', 'paper']
-# for noun in testNoun:
-#     noun = standanizeNoun(noun)
-#     print noun, cpdRatio(wn.synsets(noun)[0],1, noun)
+testNoun = ['guitar', 'apples', 'piano', 'drum', 'peach', 'grape', 'hammer', 'saw', 'screwdriver', 'pants', 'paper']
+for noun in testNoun:
+    noun = standanizeNoun(noun)
+    print noun, cpdRatio(wn.synsets(noun)[0],1, noun)
 
 #input: array and compare function
 #output: a sorted array
@@ -81,18 +90,17 @@ def getListOfNounsWithCompoundNounsFirst(NOUNS, SORTED_NOUNS):
     pickle.dump(result, output)
     output.close()
 
-getListOfNounsWithCompoundNounsFirst('all-wordnet-nouns.txt', "all-wordnet-nouns-SORTED.txt")
+#getListOfNounsWithCompoundNounsFirst('all-wordnet-nouns.txt', "all-SORTED-wordnet-nouns.txt")
 
-# which each nouns, get all hypernym/hyponyms (6 levels max)
-#     --> given a word, calculate number of compounds in full_hypornym -> get all hypernym/hypornym -> calculate again
-# select all word which ratio compounds > 25%
-# direct hyponyms - len(target word) >=4
-for noun in nounsInput:
+#input: noun
+#output: true if noun is bacsic word, %cpd in direct hyponym and average len direct hyponyms - len(target word)
+#
+# word which ratio compounds > 25%
+# average len direct hyponyms - len(target word) >=4
+def isABacsicWord(noun):
     noun = mod1.standanizeNoun(noun)
     cpd, hyponym, total_lens = mod1.cpdRatio(wn.synsets(noun)[0],1, noun)
     if hyponym and (int(round(float(cpd)*100/hyponym)) >= 25 and int(round(float(total_lens)/hyponym))>= 4):
-        #multiple form of noun
-        print noun, int(round(float(cpd)*100/hyponym)), len(noun), int(round(float(total_lens)/hyponym))
-    # if hyponym:
-    #     print noun, int(round(float(cpd)*100/hyponym)), len(noun), int(round(float(total_lens)/hyponym))
-# calculate this ratio
+        True, int(round(float(cpd)*100/hyponym)), -len(noun) + int(round(float(total_lens)/hyponym))
+    else:
+        False, int(round(float(cpd)*100/hyponym)), -len(noun) + int(round(float(total_lens)/hyponym))
