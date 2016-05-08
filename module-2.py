5a12d3592bb48e3308b36353c563369e367d236b
Hùng Minh Trần <Hng Minh Trn>
2016-05-08 23:47:11 +0700

    checked the regex

--- module-2.py
+++ module-2.py
@@ -4,18 +4,25 @@ from nltk.corpus import wordnet as wn
 import nltk
 import re
 
+#result to compare:
+#easy text:   ratio:  39.1304347826 % ['can', 'cold', 'hold', 'orange', 'plain', 'potato', 's', 'vinegar', 'well']
+#medium text: ratio:  28.0          % ['course', 'culture', 'mean', 'right', 's', 'technology', 'trade']
+#hard text:   ratio:  24.2424242424 % ['barometer', 'call', 'office', 'out', 'perfect', 'street', 'system', 'well']
 def findAllItemFromArray(inputData, searchData):
-	result = []
-	for noun in searchData:
-	    # if re.search(r'\b' + noun + r'\b', inputData):
-	    #     result.append(noun)
-	    #     re.sub(r'\b' + noun + r'\b', '', inputData)
-	    if re.search(r''+noun, inputData):
-	        result.append(noun)
-	        re.sub(r''+noun, '', inputData)
-	return result
+    result = []
+    # complile first to make runtime faster when using too many time
+    # prog = re.compile(pattern)
+    # result = prog.match(string)
+    for noun in searchData:
+        temp = r'\b' + noun + r'\b'
+        if (re.search(temp, inputData) != None):
+            result.append(noun)
+            print temp
+            # re.sub(r'\b' + noun + r'\b', ' ', inputData) #avoid concat string can be created new noun
+        # if inputData.find(noun) > -1: # faster
+    return result
 
-INPUT = 'data/easy.txt'
+INPUT = 'data/medium.txt'
 BLW_NOUNS = 'all-BLW.txt'
 NOUNS = 'all-SORTED-wordnet-nouns.txt'
 # get input
@@ -40,3 +47,9 @@ nounsInput = findAllItemFromArray(tmp, NounsArray.splitlines())
 # print nounsBLWInput
 # print nounsInput
 print "ratio: ", float(len(nounsBLWInput))/len(nounsInput)*100, "%"
+print "blw:"
+print nounsBLWInput
+print "all nouns:"
+print nounsInput
+print inputData
+print re.search(inputData, '/bre/b')
