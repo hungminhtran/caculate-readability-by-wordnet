29c672042f81aed0078f784bee4bd2e9dd976bfa
Tran Minh Hung <minhhung.1301@gmail.com>
2016-05-27 15:35:01 +0700

    add new module, new input data

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python test_ExtractText.py
 # python module-2.py 4 data/testDataTEI data/output.csv input/wn-nouns/all-BLW.txt input/wn-nouns/all-SORTED-wordnet-nouns.txt
-#python module-2.py 4 'data/English Textbook 4 Readability Level' data/ET4RL-outputWN.csv input/wn-nouns/all-BLW.txt input/wn-nouns/all-SORTED-wordnet-nouns.txt
-python module-2.py 4 'data/English Textbook 4 Readability Level' data/ET4RL-outputFreq.csv input/freq-nouns/all-BLW.txt input/freq-nouns/words_freq_SORTED.txt
+#python module-2.py 4 'data/English Textbook 4 Readability Level' output/ET4RL-outputWN.csv input/wn-nouns/all-BLW.txt input/wn-nouns/all-SORTED-wordnet-nouns.txt
+python module-2.py 4 'data/English Textbook 4 Readability Level' output/ET4RL-outputFreq.csv input/freq-nouns/all-BLW.txt input/freq-nouns/words_freq_SORTED.txt
