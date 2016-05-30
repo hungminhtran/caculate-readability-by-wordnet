835bc2e25a5b3bce5dc33c4129625a6be51ed67c
Tran Minh Hung <minhhung.1301@gmail.com>
2016-05-30 14:59:57 +0700

    process with only POS file

--- get-nouns-and-label.py
+++ get-nouns-and-label.py
@@ -4,12 +4,16 @@ import re
 #run command
 #python get-nouns-and-label.py 'input/POS-nouns' 'output/POS-output.txt'
 
+print 'can only find *.POS file'
+print ''
 mod1 = importlib.import_module("module-1")
 outputFile = open(sys.argv[2], 'w+')
 for f in mod1.listAllFile(sys.argv[1], 1):
-    print 'process file ', f
-    inputData = open(f, 'r').read()
-    temp = re.findall("[a-zA-Z1-9]+/[A-Z]+", inputData)
-    outputFile.write("\n".join(temp) + "\n")
-    print ''
+    if (re.search("\.POS$", f) != None):
+        print 'process file ', f
+        inputData = open(f, 'r').read()
+        temp = re.findall("[a-zA-Z1-9]+/[A-Z]+", inputData)
+        outputFile.write("\n".join(temp) + "\n")
+    else:
+        print "no pos file in ", f
 outputFile.close()
