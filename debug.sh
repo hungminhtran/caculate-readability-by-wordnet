83bf0af8e641476de5a3c3227239e7618febe7ab
Tran Minh Hung <minhhung.1301@gmail.com>
2016-06-05 00:59:47 +0700

    sort dataset, fix bug when processing vn

---
+++ debug.sh
@@ -0,0 +1,4 @@
+# for debug
+echo "just debugging"
+# python3 main.py 4 data/testDataTEI output/test_output.csv input/wn-nouns/all-wn-BLW.txt input/wn-nouns/all-wn-SORTED-nouns.txt 0 1
+python3 main.py 2 data/testDataVietnamese output/test_Vietnamese_output.csv input/vietnamesewn-nouns/all-vietnamesewn-BLW.txt input/vietnamesewn-nouns/all-vietnamesewn-SORTED-nouns.txt 0 0
