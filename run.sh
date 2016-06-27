4e95636c2a78f2af7ee3922d6826f911c1428eaf
Tran Minh Hung <minhhung.1301@gmail.com>
2016-06-27 08:07:00 +0700

    rename data, add new code to run

--- run.sh
+++ run.sh
@@ -6,5 +6,9 @@ echo "program running"
 #python3 main.py 4 'data/English Textbook 4 Readability Level' output/ET4RL-outputBlw.csv input/blw-nouns/all-blw-BLW.txt input/wn-nouns/all-wn-SORTED-nouns.txt  0 1
 #python3 main.py 4 'data/English Textbook 4 Readability Level' output/ET4RL-outputWN.csv input/wn-nouns/all-wn-BLW.txt input/wn-nouns/all-wn-SORTED-nouns.txt  0 1
 #python3 main.py 4 'data/English Textbook 4 Readability Level' output/ET4RL-outputFreq.csv input/freq-nouns/3000-freq-BLW.txt input/wn-nouns/all-wn-SORTED-nouns.txt  0 1
-python3 main.py 4 data/Vietnamese_by_catalog/Easy/ output/vietnamesewn_Easy_output.csv input/vietnamesewn-nouns/all-vietnamesewn-BLW.txt input/vietnamesewn-nouns/all-vietnamesewn-SORTED-nouns.txt 0 0
-python3 main.py 4 data/Vietnamese_by_catalog/Normal output/vietnamesewn_Normal_output.csv input/vietnamesewn-nouns/all-vietnamesewn-BLW.txt input/vietnamesewn-nouns/all-vietnamesewn-SORTED-nouns.txt 0 0
+# python3 main.py 4 data/Vietnamese_by_catalog/Easy/ output/vietnamesewn_Easy_output.csv input/vietnamesewn-nouns/all-vietnamesewn-BLW.txt input/vietnamesewn-nouns/all-vietnamesewn-SORTED-nouns.txt 0 0
+# python3 main.py 4 data/Vietnamese_by_catalog/Normal output/vietnamesewn_Normal_output.csv input/vietnamesewn-nouns/all-vietnamesewn-BLW.txt input/vietnamesewn-nouns/all-vietnamesewn-SORTED-nouns.txt 0 0
+python classifier.py 'test_data.txt' 'output/test_Vietnamese_output_classifier.csv' -1 'data/test_TanSoTu.txt'
+python classifier.py 'Difficult_data.txt' 'output/vietnamesewn_Difficult_output.csv' 3 'data/TanSoTu.txt'
+python classifier.py 'Easy_data.txt' 'output/vietnamesewn_Easy_output.csv' 1 'data/TanSoTu.txt'
+python classifier.py 'Normal_data.txt' 'output/vietnamesewn_Normal_output.csv' 2 'data/TanSoTu.txt'
