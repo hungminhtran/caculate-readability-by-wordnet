9476de183c5e4b7a2aa647679a290a9c8c81dd22
Tran Minh Hung <minhhung.1301@gmail.com>
2016-07-04 11:08:43 +0700

    remove incorrect data

--- classifier_trainning.py
+++ classifier_trainning.py
@@ -10,7 +10,43 @@ from multiprocessing import Pool
 MAX_PROCESS = 4
 CF = 0.9
 
-def smv_freq(inputFile, directory = 'svm_pkl/', mykernel=['linear', 'poly', 'rbf'], isuseDict = 0, dictPath = 'data/TanSoTu.txt'):
+def train(clf, X, clf_name, processID, clfQueue, PROCESS_LOCK):
+    PROCESS_LOCK.acquire()
+    print('='*5, 'proc', processID, 'start at', datetime.now().time(), '='*5)    
+    PROCESS_LOCK.release()    
+    while (1):
+        #get a file from queue
+        PROCESS_LOCK.acquire()
+        #check if queue is empty
+        if clfQueue.empty():
+            PROCESS_LOCK.release()
+            break
+        else:
+            _i = clfQueue.get()
+            PROCESS_LOCK.release()
+            #begin calculate
+            startTime = time.time();
+            beginTime = datetime.now().time()
+
+            temp1 = int(CF*len(X[0])) - 1
+            temp2 = len(X[0][0]) - 1 - (j+1) % 2
+            # print(X[:temp1,:temp2], X[:temp1, -1])
+            clf[_i].fit(X[0][:temp1,:temp2], X[0][:temp1, -1])
+            joblib.dump(clf_List[j],  directory + '/' + clf_name[j] + '.pkl')
+
+            endTime = time.time();
+            #write down\
+            PROCESS_LOCK.acquire()
+            TOTAL_TIME[1] = TOTAL_TIME[1] + endTime - startTime
+            print(int(TOTAL_TIME[0]), "-proc", processID, 'start time', beginTime, 'end process time', datetime.now().time(), "-time cost:", int(time.time() - startTime), clf_name[_i], X[0][:temp1,:temp2].shape, temp1, temp2, _i)
+            TOTAL_TIME[0] = TOTAL_TIME[0] + 1
+            PROCESS_LOCK.release()
+    PROCESS_LOCK.acquire()
+    print('='*5, '-proc', processID, 'exit at', datetime.now().time(), '='*5)
+    PROCESS_LOCK.release()
+
+
+def smv_freq(inputFile, directory = 'svm_pkl/', mykernel=['linear'], isuseDict = 0, dictPath = 'data/TanSoTu.txt'):
     START_TIME = time.time()
     if (isuseDict):
         dictFreq = []
@@ -59,16 +95,30 @@ def smv_freq(inputFile, directory = 'svm_pkl/', mykernel=['linear', 'poly', 'rbf
     print('train and dump clf')  
     if not os.path.exists(directory):
         os.makedirs(directory)
-    for j in range(len(clf_List)):
-        TRAIN_TIME = time.time()
-        temp1 = int(CF*len(X)) - 1
-        temp2 = len(X[0]) - 1 - (j+1) % 2
-        # print(X[:temp1,:temp2], X[:temp1, -1])
-        clf_List[j].fit(X[:temp1,:temp2], X[:temp1, -1])
-        print(X[:temp1,:temp2].shape, temp1, temp2, j, 'clf done', 'time', time.time() - TRAIN_TIME)
-        DUMP_TIME = time.time()
-        joblib.dump(clf_List[j],  directory + '/' + clf_name[j] + '.pkl')
-        print(clf_name[j], 'dump time total: ', time.time() - DUMP_TIME)
+    # for j in range(len(clf_List)):
+    #     TRAIN_TIME = time.time()
+    #     temp1 = int(CF*len(X)) - 1
+    #     temp2 = len(X[0]) - 1 - (j+1) % 2
+    #     # print(X[:temp1,:temp2], X[:temp1, -1])
+    #     clf_List[j].fit(X[:temp1,:temp2], X[:temp1, -1])
+    #     print(X[:temp1,:temp2].shape, temp1, temp2, j, 'clf done', 'time', time.time() - TRAIN_TIME)
+    #     DUMP_TIME = time.time()
+    #     joblib.dump(clf_List[j],  directory + '/' + clf_name[j] + '.pkl')
+    #     print(clf_name[j], 'dump time total: ', time.time() - DUMP_TIME)
+    myProcess = []
+    lock = Lock()
+    TOTAL_TIME = Array('f', [0, 0])
+    outQueue = SimpleQueue()
+    for processID in range(MAX_PROCESS):
+        myProcess.append(Process(target=generateOutput, args=(processID, filesQueue, outQueue, lock, TOTAL_TIME,
+        INPUT_BLW_NOUNS, INPUT_ALL_NOUNS, DEBUG, TEIFILE)))
+    # write output process
+    myProcess.append(Process(target=writeOUt, args=(outQueue, outputPath, lock)))
+    #start and wait all process finish their job
+    for _process in myProcess:
+        _process.start()
+    for _process in myProcess:
+        _process.join()
 
     print('test')
     for j in range(len(clf_List)):
@@ -91,4 +141,4 @@ if __name__ == '__main__':
     # smv_freq(inputFile_1, directory = 'svm_pkl/', mykernel=['linear'], isuseDict = 1, dictPath = 'data/TanSoTu.txt')
     inputFile_2 = ['data/ppVietnameseShallowFt/ShallowFt_Difficult_data.txt', 'data/ppVietnameseShallowFt/ShallowFt_Normal_data.txt', 'data/ppVietnameseShallowFt/ShallowFt_Easy_data.txt']
     # smv_freq(inputFile_2, 'svm_pkl_4ft/', mykernel=['linear', 'linear', 'linear'])
-    smv_freq(inputFile_2, 'svm_pkl_4ft/')
+    smv_freq(inputFile_2, 'svm_pkl_4ft/', mykernel=['linear', 'rbf'])
